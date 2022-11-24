dfda1220c93046f6f4e6327df010cf678726b0cc
loneliness-mahadi <lonelinessmahadi@gmail.com>
2022-11-24 18:06:23 +0600

    get collection

--- index.js
+++ index.js
@@ -13,14 +13,33 @@ app.use(cors())
 app.use(express.json())
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERNAME}@cluster0.hlyc9ph.mongodb.net/?retryWrites=true&w=majority`;
+const uri = " mongodb://localhost:27017"
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERNAME}@cluster0.hlyc9ph.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+async function run() {
+    try {
+
+        const catagoryCollections = client.db('Book-House').collection('Catagorys')
+
+        app.get('/catagorys', async (req, res) => {
+            const query = {}
+            const result = await catagoryCollections.find(query)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(er => console.log(er))
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('This site is running now')
 })
 
 app.listen(port, () => {
